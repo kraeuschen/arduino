003b2dc59c021a917a0a28e2c685b51392eb1f73
Jan Krause <jan.krause@wywy.com>
2013-01-22 01:13:03 +0100

    Traffic Light - Extreme feedback for jenkins
    There are three leds. The python scripts sends pull request
    to an jenkins, gets the job and switch the light.
    
    You need jenkinsapi for that.

---
+++ jenkins_traffic_light/traffic_light.ino
@@ -0,0 +1,62 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+ 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int ledGreen = 13;
+int ledYellow = 10;
+int ledRed = 7;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(ledGreen, OUTPUT);     
+  pinMode(ledYellow, OUTPUT);   
+  pinMode(ledRed, OUTPUT);   
+  Serial.begin(9600);
+}
+
+void reset() {
+  digitalWrite(ledGreen, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(ledYellow, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(ledRed, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);  
+}
+  
+void setFail() {
+  digitalWrite(ledRed, HIGH);   
+}
+
+void setSuccess() {
+  digitalWrite(ledGreen, HIGH);   
+}
+
+void setBuild() {
+  digitalWrite(ledYellow, HIGH);   
+}
+
+void switchLight(char incomingChar) {
+  reset();
+  
+  if (incomingChar == 's') {
+    setSuccess();
+  } else if (incomingChar == 'b') {
+    setBuild();
+  } else if (incomingChar == 'f') {
+    setFail();
+  }
+  
+  delay(1000);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  if (Serial.available() > 0) {
+    char incomingChar = Serial.read();
+    switchLight(incomingChar);
+  }  
+}
